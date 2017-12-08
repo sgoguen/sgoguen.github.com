4dea520ec30e84330e101da7df52012a5568cbb3
Steve Goguen <sgoguen@gmail.com>
2017-12-07 20:30:52 -0500

    Added todoFinder

---
+++ Journal/2017/12/todoFinder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace todoFinder
+{
+    class Program
+    {
+        //  This is a little program that searches the folder looking for TODOs
+        //  in comments.
+        static void Main(string[] args)
+        {
+            //  First, we get the patch we want to search from the command-line
+            //  arguments
+            string path = args[0] ?? "./";
+
+            //  Then we create a variable that we're going to use to track the
+            //  number of TODOs we find.
+            int todoCount = 0;
+
+            //  We're going to capture all the files in the folder
+            foreach (var file in System.IO.Directory.EnumerateFiles(path)) {
+
+                //  We'll announce each file we're going to search
+                Console.WriteLine($"Searching file: {file}");
+
+                //  Then we'll loop over the lines
+                foreach(var line in File.ReadAllLines(file)) {
+
+                    //  If the line contains our, TODO
+                    if(line.Contains("TODO:")) {
+                        
+                        //  Increment the counter
+                        todoCount += 1;
+
+                        //  And print it...
+                        Console.WriteLine($"Found {file}: {line}");
+                    }
+                }
+            }
+
+            //  When we're finished, we say we're done and tell the user
+            //  how many lines we found.
+            Console.WriteLine($"Done - Found {todoCount}!");
+
+            //  Now...  Let's convert this into F#!!!
+        }
+    }
+}
